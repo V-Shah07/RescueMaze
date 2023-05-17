f04aa71b583034f6f9413b56a15244fec79ffbee
anddai343 <123331248+anddai343@users.noreply.github.com>
2023-05-16 21:23:02 -0400

    committing

--- trackingInfo.cpp
+++ trackingInfo.cpp
@@ -11,7 +11,7 @@ Maze::Maze() :robot("wheel2 motor", "wheel1 motor",
 	"dLeft1", "dLeft2",
 	"dRight1", "dRight2",
 	"dBack1", "dBack2",
-	"colourSensor", "LCam", "RCam", "inertialSensor", "gps", "lidar",
+	"colourSensor", "LCam", "RCam", "inertialSensor", "gps", "lidarSensor",
 	"emitter", "receiver")
 {
 	tracker = Tracker();
@@ -45,46 +45,8 @@ void Maze::insert_border(Border border, Direction dir)
 	}
 }
 
-int sin(Direction dir)
-{
-	switch (dir)
-	{
-	case Up:
-		return 1;
-
-	case Right:
-		return 0;
-
-	case Down:
-		return -1;
-
-	case Left:
-		return 0;
-	}
-}
-int cosin(Direction dir)
-{
-	switch (dir)
-	{
-	case Up:
-		return 0;
-	case Right:
-		return 1;
-	case Down:
-		return 0;
-	case Left:
-		return -1;
-	}
-}
-
-void translate(Direction dir)
-{
-	
-}
 void Maze::update()
 {
-
-	/*
 	Border left = Empty, right = Empty, up = Empty, down = Empty;
 
 	double upD, downD, leftD, rightD;
@@ -93,19 +55,20 @@ void Maze::update()
 	leftD = robot.getDist(relToAbs(tracker.direction, Left));
 	rightD = robot.getDist(relToAbs(tracker.direction, Right));
 
-	if (upD < 12.0)
+	double dist = 16.0;
+	if (upD < dist)
 	{
 		up = Wall;
 	}
-	if (downD < 12.0)
+	if (downD < dist)
 	{
 		down = Wall;
 	}
-	if (leftD < 12.0)
+	if (leftD < dist)
 	{
 		left = Wall;
 	}
-	if (rightD < 12.0)
+	if (rightD < dist)
 	{
 		right = Wall;
 	}
@@ -121,25 +84,6 @@ void Maze::update()
 	cout << "Right Dist: " << rightD << endl;
 	robot.delay(1000);
 	//cout << "Left: " << map[tracker.y][tracker.x].left << endl;
-	*/
-
-
-	const LidarPoint* lidarPoints = robot.lidarFuncs();
-
-	for (int i = 0; i < 360; i++)
-	{
-		cout << "X: " << lidarPoints[i].x/0.12 << " Y: " << lidarPoints[i].y/0.12 << "Z: " << lidarPoints[i].z/0.12 << endl ;
-	}
-
-
-	int localX = tracker.x;
-	int localY = tracker.y;
-
-	Direction dir = tracker.direction;
-	int translatedX = localX * cosin(dir) - localY * sin(dir);
-	int translatedY = localX * sin(dir) + localY * cosin(dir);
-
-	
 }
 
 bool traversable(Tile map[100][100], int x, int y, Direction dir)
@@ -149,19 +93,11 @@ bool traversable(Tile map[100][100], int x, int y, Direction dir)
 	{
 		return true;
 	}
-	if ((map[y][x].getBorder(dir) == Empty) && !map[y + p.y][x + p.x].redTile)
-	{
-		return true;
-	}
-	if ((map[y][x].getBorder(dir) == Empty) && !map[y + p.y][x + p.x].greenTile)
-	{
-		return true;
-	}
 	return false;
 }
 vector<Direction> Maze::BFS()
 {
-	
+
 	struct State
 	{
 		Pos p;
@@ -185,12 +121,12 @@ vector<Direction> Maze::BFS()
 	//cout << "After Update(): ";
 	//cout << map[tracker.y][tracker.x] << endl;
 	//robot.delay(1000);
-	
+
 
 	//robot.delay(1000);
 	//cout << pos << endl;
 
-	
+
 	while (!toSearch.empty()) {
 		State curState = toSearch.front(); //get tile to search
 		Tile t = map[curState.p.y][curState.p.x];
@@ -198,8 +134,8 @@ vector<Direction> Maze::BFS()
 		//cout << "Tile to search: " << t << endl;
 
 		//if the tile ur searching for is not visited, or the tile ur searching for is not start tile
-		if ((!allTilesVisited && !t.visited) || 
-			(allTilesVisited && ((curState.p.x == tracker.startX) && (curState.p.y == tracker.startY)))) 
+		if ((!allTilesVisited && !t.visited) ||
+			(allTilesVisited && ((curState.p.x == tracker.startX) && (curState.p.y == tracker.startY))))
 		{
 			//cout << "Going to tile " << curState.p << endl;
 
@@ -235,7 +171,7 @@ vector<Direction> Maze::BFS()
 		}
 
 	}
-	
+
 	allTilesVisited = true;
 	return {};
 }
