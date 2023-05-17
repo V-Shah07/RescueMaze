224f89b0076faa07e65471491b02481afdd5a80a
anddai343 <123331248+anddai343@users.noreply.github.com>
2023-05-16 21:16:05 -0400

    fixed a little bit

--- RescueMaze.cpp
+++ RescueMaze.cpp
@@ -138,7 +138,7 @@ int main()
     signs_and_victims val, leftval;
     while (maze.robot.getTimeStep() != -1) {
         //maze.robot.turn(-90);
-       //cout << "Facing: " << radToDeg(maze.robot.getYaw()) << endl;
+        //cout << "Facing: " << radToDeg(maze.robot.getYaw()) << endl;
         //auto coords = maze.robot.getCoords();
         //cout << coords.x << " " << coords.y << " " << coords.z << endl;
 
@@ -156,8 +156,14 @@ int main()
         {
             maze.robot.exit_maze();
         }
-    }
 
+        val = maze.robot.getSign(Right);
+        leftval = maze.robot.getSign(Left);
+        printf("r: %s \n", maze.robot.printSign(val));
+        printf("l: %s \n", maze.robot.printSign(leftval));
+        //maze.robot.lidarFuncs();
+        //cout << "Left: " << maze.robot.printSign(maze.robot.getSign(Left)) << endl;
+        //cout << "Right: " << maze.robot.printSign(maze.robot.getSign(Right)) << endl;
+    }
     return 0;
 }
-
